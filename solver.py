28a0690ecfe8cefc211c5c0b4fa588f14c759d8f
hargunmujral <hargunmujral@gmail.com>
2024-05-12 13:00:40 -0700

    Refactored to make into cli tool

--- solver.py
+++ solver.py
@@ -1,93 +1,71 @@
+import argparse
 
+class Solver24:
+    def __init__(self, nums):
+        self.nums = nums
 
-def build_permutations(nums):
-    permutations = []
-    for a in nums:
-        for b in nums:
-            if b == a:
-                continue
-            for c in nums:
-                if c in [a, b]:
+    def build_permutations(self):
+        permutations = []
+        for a in self.nums:
+            for b in self.nums:
+                if b == a:
                     continue
-                for d in nums:
-                    if d in [a, b, c]:
+                for c in self.nums:
+                    if c in [a, b]:
                         continue
-                    permutations.append([a, b, c, d])
-    return permutations
+                    for d in self.nums:
+                        if d in [a, b, c]:
+                            continue
+                        permutations.append([a, b, c, d])
+        return permutations
 
+    def build_ops(self):
+        ops = ['+', '-', '*', '/']
+        ops_permutations = []
+        for a in ops:
+            for b in ops:
+                for c in ops:
+                    ops_permutations.append([a, b, c])
+        return ops_permutations
 
-def build_ops():
-    ops = ['+', '-', '*', '/']
-    ops_permutations = []
-    for a in ops:
-        for b in ops:
-            for c in ops:
-                ops_permutations.append([a, b, c])
-    return ops_permutations
+    def solve(self):
+        permutations = self.build_permutations()
+        ops_permutations = self.build_ops()
+        res = set()
 
+        for p in permutations:
+            for ops in ops_permutations:
+                a, b, c, d = p
+                op1, op2, op3 = ops
 
+                expressions = [
+                    f'(({a} {op1} {b}) {op2} {c}) {op3} {d}',
+                    f'({a} {op1} ({b} {op2} {c})) {op3} {d}',
+                    f'{a} {op1} (({b} {op2} {c}) {op3} {d})',
+                    f'{a} {op1} ({b} {op2} ({c} {op3} {d}))',
+                    f'({a} {op1} {b}) {op2} ({c} {op3} {d})',
+                    f'{a} {op1} {b} {op2} {c} {op3} {d}'
+                ]
 
+                for expr in expressions:
+                    try:
+                        if eval(expr) == 24:
+                            res.add(expr)
+                    except ZeroDivisionError:
+                        pass
 
-def solve_24(nums):
-    # a, b, c, d = nums
+        print(res)
+        return res
 
-    permutations = build_permutations(nums)
-    
-    print(permutations)
-    print(len(permutations))
+def main():
+    parser = argparse.ArgumentParser(description="Let's play 24.")
+    parser.add_argument('nums', metavar='N', type=int, nargs=4,
+                        choices=range(1, 14),
+                        help='Choose 4 numbers from 1 to 13.')
+    args = parser.parse_args()
 
-    ops_permutations = build_ops()
-    
-    print(ops_permutations)
+    solver = Solver24(args.nums)
+    solver.solve()
 
-
-    res = set()
-    for p in permutations:
-        for ops in ops_permutations:
-            a, b, c, d = p
-            op1, op2, op3 = ops
-
-            # ((a . b) . c) . d
-            try:
-                if eval(f'(({a} {op1} {b}) {op2} {c}) {op3} {d}') == 24:
-                    expr = f'(({a} {op1} {b}) {op2} {c}) {op3} {d}'
-                    res.add(expr)
-                    # return True
-
-                # (a . (b . c)) . d
-                if eval(f'({a} {op1} ({b} {op2} {c})) {op3} {d}') == 24:
-                    expr = f'({a} {op1} ({b} {op2} {c})) {op3} {d}'
-                    res.add(expr)
-
-                # a . ((b . c ) . d)
-                if eval(f'{a} {op1} (({b} {op2} {c}) {op3} {d})') == 24:
-                    expr = f'{a} {op1} (({b} {op2} {c}) {op3} {d})'
-                    res.add(expr)
-
-                # a . (b . (c . d))
-                if eval(f'{a} {op1} ({b} {op2} ({c} {op3} {d}))') == 24:
-                    expr = f'{a} {op1} ({b} {op2} ({c} {op3} {d}))'
-                    res.add(expr)
-
-                # (a . b) . (c . d)
-                if eval(f'({a} {op1} {b}) {op2} ({c} {op3} {d})') == 24:
-                    expr = f'({a} {op1} {b}) {op2} ({c} {op3} {d})'
-                    res.add(expr)
-                
-                # a . b . c . d
-                if eval(f'{a} {op1} {b} {op2} {c} {op3} {d}') == 24:
-                    expr = f'{a} {op1} {b} {op2} {c} {op3} {d}'
-                    res.add(expr)
-            except ZeroDivisionError:
-                pass
-    print(res)
-    
-        
-        
-
-
-
-solve_24([1, 2, 3, 4])
-solve_24([6, 4, 1, 3])
-
-
+if __name__ == '__main__':
+    main()
